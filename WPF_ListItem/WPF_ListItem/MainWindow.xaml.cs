fded27f7aa0461b843c1a76d8c5640fcd9779bf7
Brian Hwang <brianhwang29@gmail.com>
2023-11-24 15:52:44 +0900

    dynamic list item

---
+++ WPF_ListItem/WPF_ListItem/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WPF_ListItem.Data;
+
+namespace WPF_ListItem
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public List<Person> People = new List<Person>()
+        {
+            new Person{Name="Brian", Age=30}, 
+            new Person{Name="Jenny", Age=54},
+            new Person{Name="May", Age=35},
+            new Person{Name="Ali", Age=20},
+            new Person{Name="Max", Age=10},
+            
+        };
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            ListBoxPeople.ItemsSource = People;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedItems = ListBoxPeople.SelectedItems;
+            foreach(Person person in selectedItems)
+            {
+                MessageBox.Show(person.Name);
+            }
+        }
+    }
+}

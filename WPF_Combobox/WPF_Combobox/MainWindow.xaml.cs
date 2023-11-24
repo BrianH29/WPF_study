6b027452fcb6ca9911864e8e78351e301d84a6ff
Brian Hwang <brianhwang29@gmail.com>
2023-11-24 09:50:52 +0900

    using combobox

---
+++ WPF_Combobox/WPF_Combobox/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Combobox
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // BH - you could get a whole list of colors
+            //comboBoxColors.ItemsSource = typeof(Colors).GetProperties();
+
+            // BH - just option of my own 
+            List<Colors> colors = new List<Colors>();
+            colors.Add(new Colors() { ColorName = "red"});
+            colors.Add(new Colors() { ColorName = "blue"});
+            colors.Add(new Colors() { ColorName = "yellow"});
+
+            comboBoxColors.ItemsSource = colors;
+        }
+    }
+
+    public class Colors
+    {
+        public string ColorName { get; set; }
+    }
+     
+}
